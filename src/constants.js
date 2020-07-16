34cdd6bb3b28c188612eddca2d1a43ad5b4b138d
punitpratapsingh <punitpratapsingh30@gmail.com>
2020-07-16 20:55:56 +0530

    cleanups

--- src/constants.js
+++ src/constants.js
@@ -5,13 +5,12 @@ import { SERVER } from './statics';
 
 export const SERVER_BASE_URL = SERVER;
 export const APPLICATION_ROUTES = {
-	// URLS
+	// URLS for server
     USER_LOGIN: `${SERVER_BASE_URL}users/login`,
     USER_REGISTER: `${SERVER_BASE_URL}users/register`,
     LIST_MOVIES: `${SERVER_BASE_URL}movies/list`,
     SEARCH_MOVIES: `${SERVER_BASE_URL}movies/search`,
 };
-
 export const navigationIndexer = {
 	dashboard: 1,
 };
