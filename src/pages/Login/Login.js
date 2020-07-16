0653b84348b2bbddc561b7af6abedf7befa33f71
Sahil <sharma.1901@yahoo.com>
2020-07-16 18:15:46 +0530

    Login/Register Frontend

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React from "react";
 
 import styles from "./Login.module.css";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-    return (
-        <div>
-            Login Page
-        </div>
-    );
+  return (
+    <div className={styles.loginPageContainer}>
+        <Link to="/" className={styles.revertBack}>Go Back</Link>
+      <form className={styles.loginForm}>
+          <h1 className={styles.heading}>Login</h1>
+          <input className={styles.inputField} type="email" placeholder="Email Address" name="email"/>
+          <input className={styles.inputField} type="password" placeholder="Password" name="password"/>
+          <button type="submit" className={styles.loginBtn}>Login</button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,13 +1,42 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 import styles from "./Register.module.css";
 
 const Register = () => {
-    return (
-        <div>
-            Register Page
-        </div>
-    );
+  return (
+    <div>
+      <div className={styles.registerPageContainer}>
+        <Link to="/" className={styles.revertBack}>
+          Go Back
+        </Link>
+        <form className={styles.registerForm}>
+          <h1 className={styles.heading}>Register</h1>
+          <input
+            className={styles.inputField}
+            type="name"
+            placeholder="Your Name"
+            name="name"
+          />
+          <input
+            className={styles.inputField}
+            type="email"
+            placeholder="Email Address"
+            name="email"
+          />
+          <input
+            className={styles.inputField}
+            type="password"
+            placeholder="Password"
+            name="password"
+          />
+          <button type="submit" className={styles.registerBtn}>
+            Register
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
